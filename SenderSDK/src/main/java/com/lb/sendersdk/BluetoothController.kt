936746a569309fc62901d5f9e956a6e1f6365b7b
gerardogonzalezdocs <gerardogonzalezdocs@gmail.com>
2025-11-03 17:31:36 -0600

    Test

---
+++ SenderSDK/src/main/java/com/lb/sendersdk/BluetoothController.kt
@@ -0,0 +1,11 @@
+package com.lb.sendersdk
+
+import android.util.Log
+
+class BluetoothController {
+
+    fun connect(){
+        Log.d("BluetoothController", "connect")
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "LebentechSenderSDK"
 include(":app")
+include(":SenderSDK")
